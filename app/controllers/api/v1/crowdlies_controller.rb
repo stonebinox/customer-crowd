c482c786a6c9916ee138cc39d7b0e66c86462e8a
Samuel Afolaranmi <rafolaranmi@gmail.com>
2017-10-30 22:21:38 +0100

    Add Auth

--- app/controllers/api/v1/crowdlies_controller.rb
+++ app/controllers/api/v1/crowdlies_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::CrowdliesController < ApplicationController
 
 	respond_to :json
+	skip_before_action  :verify_authenticity_token
 
   	def index
   		respond_with Crowdly.where(user_id: params[:id])
