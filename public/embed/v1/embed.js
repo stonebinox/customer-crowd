b53a823ec8181005904fbca8eff39393d0c5c215
Samuel Afolaranmi <rafolaranmi@gmail.com>
2017-11-02 14:52:53 +0100

    embed

--- public/embed/v1/embed.js
+++ public/embed/v1/embed.js
@@ -1,16 +1,11 @@
-function getData(){
-    $.ajax({
-		url:"http://customer-crowd.herokuapp.com/api/v1/crowdlies/1",
-		jsonp: "callback",
-		dataType: "jsonp",
-		data:{
-			format: "json"
-		},
-        error: function(xhr,stat,err){
-            console.log("Network error");
-        },
-        success:function(responseText){
-            setTimeout(function(){
+var app=angular.module("ccrowd",[]);
+app.controller("data",function($scope,$http){
+	$scope.getData=function(){
+		$http.get("http://customer-crowd.herokuapp.com/api/v1/crowdlies/1")
+		.then(function success(response){
+			response=response.data;
+			//the above var would be json automatically
+			setTimeout(function(){
                 var data=responseText;
                 for(var i=0;i<data.length;i++){
                     var row=data[i];
@@ -32,9 +27,49 @@ function getData(){
                     //messageBox(title,content);
                 }   
             },10000);  
-		}
-    });
-}
+		},
+		function error(response){
+			console.log("Network error");
+		});
+	};
+});
+// function getData(){
+//     $.ajax({
+// 		url:"http://customer-crowd.herokuapp.com/api/v1/crowdlies/1",
+// 		jsonp: "callback",
+// 		dataType: "jsonp",
+// 		data:{
+// 			format: "json"
+// 		},
+//         error: function(xhr,stat,err){
+//             console.log("Network error");
+//         },
+//         success:function(responseText){
+//             setTimeout(function(){
+//                 var data=responseText;
+//                 for(var i=0;i<data.length;i++){
+//                     var row=data[i];
+//                     var id=row.id;
+//                     var userID=row.user_id;
+//                     var name=row.name;
+//                     var image=row.image;
+//                     var imageURL=image.url;
+//                     var bio=row.bio;
+//                     var fb=row.facebook;
+//                     var twitter=row.twitter;
+//                     var insta=row.instagram;
+//                     var linkedin=row.linkedin;
+//                     var review=row.review;
+//                     var createdAT=row.created_at;
+//                     var updatedAt=row.updated_at;
+//                     //build ui content here. 
+//                     //var content='<div>content here</div>';
+//                     //messageBox(title,content);
+//                 }   
+//             },10000);  
+// 		}
+//     });
+// }
 function messageBox(title,content,sizeFlag){
     title=$.trim(title);
 	var modal=document.getElementById("myModal");
