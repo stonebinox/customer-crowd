f8ae21ac978f69c725318f7a9cff8d3be48fd5c1
Samuel Afolaranmi <rafolaranmi@gmail.com>
2017-11-04 08:42:38 +0100

    embed

--- public/embed/v1/embed.js
+++ public/embed/v1/embed.js
@@ -1,31 +1,37 @@
-$http.jsonp("http://customer-crowd.herokuapp.com/api/v1/crowdlies/1?callback=JSON_CALLBACK")
-	.then(function success(response){
-		response=response.data;
-		//the above var would be json automatically
-		setTimeout(function(){
-            var data=response;
-            for(var i=0;i<data.length;i++){
-                var row=data[i];
-                var id=row.id;
-                var userID=row.user_id;
-                var name=row.name;
-                var image=row.image;
-                var imageURL=image.url;
-                var bio=row.bio;
-                var fb=row.facebook;
-                var twitter=row.twitter;
-                var insta=row.instagram;
-                var linkedin=row.linkedin;
-                var review=row.review;
-                var createdAT=row.created_at;
-                var updatedAt=row.updated_at;
-                //build ui content here. 
-                // var content='<div><h1 style="color: #000;">'+userID+', '+name+'</h1></div>';
-                // messageBox(title,content);
-                console.log(name);
-            }   
-        },10000);  
-	},
-	function error(response){
-		console.log("Network error");
-	});
+var app=angular.module("ccrowd",[]);
+app.controller("data",function($scope,$http){
+    $scope.getData=function(){
+        console.log("here");
+        $http.jsonp("http://customer-crowd.herokuapp.com/api/v1/crowdlies/1?callback=JSON_CALLBACK")
+        .then(function success(response){
+            response=response.data;
+            //the above var would be json automatically
+            setTimeout(function(){
+                var data=response;
+                for(var i=0;i<data.length;i++){
+                    var row=data[i];
+                    var id=row.id;
+                    var userID=row.user_id;
+                    var name=row.name;
+                    var image=row.image;
+                    var imageURL=image.url;
+                    var bio=row.bio;
+                    var fb=row.facebook;
+                    var twitter=row.twitter;
+                    var insta=row.instagram;
+                    var linkedin=row.linkedin;
+                    var review=row.review;
+                    var createdAT=row.created_at;
+                    var updatedAt=row.updated_at;
+                    //build ui content here. 
+                    // var content='<div><h1 style="color: #000;">'+userID+', '+name+'</h1></div>';
+                    // messageBox(title,content);
+                    console.log(name);
+                }   
+            },10000);  
+        },
+        function error(response){
+            console.log("Network error");
+        });
+    };
+});
