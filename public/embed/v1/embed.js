3eb458773725a863905b42f8d27e53bafbcf886a
Samuel Afolaranmi <rafolaranmi@gmail.com>
2017-11-15 15:51:41 +0100

    Extracted CSS & JS

--- public/embed/v1/embed.js
+++ public/embed/v1/embed.js
@@ -22,13 +22,14 @@ $.get('https://customer-crowd.herokuapp.com/api/v1/crowdlies/1', function(data) 
                         var review = row.review;
                         myFunction();
                         $("#popup").html(`
-                            <div id="snackbar" >
+                            <div id="snackbar">
                             <div style="width: 20%; float: left;">
                                 <img src="${imageURL}" class="img-responsive" style="border-radius: 50%; width: 100px;">
                             </div>
                             <div style="width: 80%; float: right;">
                                 <h4>${name}</h4>&nbsp;${fb != "" ? `<a href="${fb}"><i class="fa fa-facebook"></i></a>` : ''}${twitter != "" ? `<a href="${twitter}"><i class="fa fa-twitter"></i></a>` : ''}${instagram != "" ? `<a href="${instagram}"><i class="fa fa-instagram"></i></a>` : ''}${linkedin != "" ? `<a href="${linkedin}"><i class="fa fa-linkedin"></i></a>` : ''}<br/>
-                                <small>${bio}</small><br/><p><strong>${review}</strong></p>
+                                <small>${bio}</small><br/><p><strong>${review}</strong></p><br/>
+                                <small>powered by <a href="http://customercrowd.com" target="_blank"><strong>CustomerCrowd</strong></a></small>
                             </div>
                             </div>
                         ` );
