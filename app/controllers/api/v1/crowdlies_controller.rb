f4546b03ff988b4d46cdf8ac285450bdd4f34863
Samuel Afolaranmi <rafolaranmi@gmail.com>
2017-11-24 02:10:37 +0100

    Headers Issue Again!

--- app/controllers/api/v1/crowdlies_controller.rb
+++ app/controllers/api/v1/crowdlies_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::CrowdliesController < ApplicationController
 
+	before_action :set_headers
 	respond_to :json
 	skip_before_action  :verify_authenticity_token
 
@@ -7,5 +8,15 @@ class Api::V1::CrowdliesController < ApplicationController
 				@user = User.find_by(token: params[:id])
   		@crowdlies = Crowdly.where(user_id: @user.id)
   		render json: @crowdlies
-  	end
+			end
+			
+			private
+			
+					def set_headers
+							headers['Access-Control-Allow-Origin'] = '*'
+							headers['Access-Control-Expose-Headers'] = 'ETag'
+							headers['Access-Control-Allow-Methods'] = 'GET'
+							headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
+							headers['Access-Control-Max-Age'] = '86400'
+					end
 end
