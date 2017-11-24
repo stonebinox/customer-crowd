45db2db5563c3d92c3e4b16b974119bdf5b8ae87
Samuel Afolaranmi <rafolaranmi@gmail.com>
2017-11-24 09:22:16 +0100

    Setting Fog to true

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,10 +1,11 @@
 CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'
   config.fog_credentials = {
       :provider               => 'AWS',
       :aws_access_key_id      => "AKIAI6OERPJ2JDICKK3Q",
       :aws_secret_access_key  => "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",
       :region                 => 'us-east-1', # Change this for different AWS region. Default is 'us-east-1'
-      :endpoint               => 'http://customercrowd.s3.amazonaws.com'
+      # :endpoint               => 'https://customercrowd.s3.amazonaws.com'
   }
   config.fog_directory  = "customercrowd"
   config.fog_public = true
