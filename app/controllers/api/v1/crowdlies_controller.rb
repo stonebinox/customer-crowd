fe88ea8db2e5025b285f8359242fc28629c18d85
Samuel Afolaranmi <rafolaranmi@gmail.com>
2017-11-24 01:45:46 +0100

    Retrieve by token

--- app/controllers/api/v1/crowdlies_controller.rb
+++ app/controllers/api/v1/crowdlies_controller.rb
@@ -3,8 +3,9 @@ class Api::V1::CrowdliesController < ApplicationController
 	respond_to :json
 	skip_before_action  :verify_authenticity_token
 
-  	def index
-  		@crowdlies = Crowdly.where(user_id: params[:id])
+			def index
+				@user = User.find_by(token: params[:id])
+  		@crowdlies = Crowdly.where(user_id: @user.id)
   		render json: @crowdlies
   	end
 end
