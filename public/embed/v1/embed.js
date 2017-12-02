0d0718dd2be84cb3c565799fca830e19fe03dd58
Samuel Afolaranmi <rafolaranmi@gmail.com>
2017-12-02 21:28:30 +0100

    dismiss link

--- public/embed/v1/embed.js
+++ public/embed/v1/embed.js
@@ -41,7 +41,7 @@ $.get('https://customer-crowd.herokuapp.com/api/v1/crowdlies/' + identifier, fun
                 </div>
 				<div style="width: 80%; float: right; margin-top: 8px;">
                     <h4 style="margin-top: -5px;">${name}</h4>&nbsp;${fb != "" ? `<a href="${fb}" target="_blank" style="color: #51bcda !important;"><i class="fa fa-facebook"></i></a>` : ''}${twitter != "" ? `<a href="${twitter}" target="_blank" style="color: #51bcda !important;"><i class="fa fa-twitter"></i></a>` : ''}${instagram != "" ? `<a href="${instagram}" target="_blank" style="color: #51bcda !important;"><i class="fa fa-instagram"></i></a>` : ''}${linkedin != "" ? `<a href="${linkedin}" target="_blank" style="color: #51bcda !important;"><i class="fa fa-linkedin"></i></a>` : ''}<br/>
-                    <small>${bio}</small><br/><p style="font-size: 13px !important;"><strong>${review}</strong></p><br/>
+                    <small>${bio}</small><br/><p style="font-size: 13px !important; text-align: right;"><strong>${review}</strong></p><br/>
                     <small>powered by <a href="http://customercrowd.com" target="_blank" style="color: #51bcda !important;"><strong>CustomerCrowd</strong></a></small>
 				</div>
 			</div>
