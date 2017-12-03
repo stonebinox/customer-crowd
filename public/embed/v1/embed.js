c39a7c3463d0a2ee3b8accac883dc0d3ff9ed9d3
Samuel Afolaranmi <rafolaranmi@gmail.com>
2017-12-03 18:52:18 +0100

    Popup Display

--- public/embed/v1/embed.js
+++ public/embed/v1/embed.js
@@ -34,14 +34,14 @@ $.get('https://customer-crowd.herokuapp.com/api/v1/crowdlies/' + identifier, fun
                         $("#popup").html(`
 			<div id="snackbar">
                 <div style="width: 20%; float: left;">
-					<img src="${imageURL}" class="img-responsive" style="border-radius: 50%; width: 100px;">
+					<img src="${imageURL}" class="img-responsive" style="border-radius: 50%; width: 50px;">
                 </div>
                 <div style="float: right; margin-right: 5px; margin-top: 5px;">
                     <small><a href="#" onclick="dismiss()">dismiss</a></small>
                 </div>
 				<div style="width: 80%; float: right; margin-top: 5px;">
                     <h4 style="margin-top: -5px;">${name}</h4>&nbsp;${fb != "" ? `<a href="${fb}" target="_blank" style="color: #51bcda !important;"><i class="fa fa-facebook"></i></a>` : ''}${twitter != "" ? `<a href="${twitter}" target="_blank" style="color: #51bcda !important;"><i class="fa fa-twitter"></i></a>` : ''}${instagram != "" ? `<a href="${instagram}" target="_blank" style="color: #51bcda !important;"><i class="fa fa-instagram"></i></a>` : ''}${linkedin != "" ? `<a href="${linkedin}" target="_blank" style="color: #51bcda !important;"><i class="fa fa-linkedin"></i></a>` : ''}<br/>
-                    <small>${bio}</small><br/><p style="font-size: 13px !important; text-align: left;">${review}</p><br/>
+                    <small>${bio}</small><br/><p style="font-size: 13px !important; text-align: left;">${review}</p>
                     <small>powered by <a href="http://customercrowd.com" target="_blank" style="color: #51bcda !important; margin-top: -10px;"><strong>CustomerCrowd</strong></a></small>
 				</div>
 			</div>
